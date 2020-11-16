507714ff15c1180603841404e8f58351477d307a
Johannes Keller <jo.keller@fz-juelich.de>
2020-11-16 14:22:52 +0100

    compiler specific changes in src.Gnu / src.Intel in Oasis intf

--- bldsva/intf_oas3/parflow/arch/JURECA/src.Gnu/amps_init.c
+++ bldsva/intf_oas3/parflow/arch/JURECA/src.Gnu/amps_init.c
@@ -101,11 +101,7 @@ int amps_Init(int *argc, char **argv[])
 /*   MPI_Init(argc, argv);*/
    CALL_oas_pfl_init(&dummy1_oas3);
    amps_mpi_initialized = TRUE;
-#ifdef __GNUC__
-   oas3Comm = MPI_Comm_f2c(__oas_pfl_vardef_MOD_localcomm);//for GNU compioers
-#else
-   oas3Comm = MPI_Comm_f2c(oas_pfl_vardef_mp_localcomm_);  //for Intel Compilers
-#endif
+   oas3Comm = MPI_Comm_f2c(__oas_pfl_vardef_MOD_localcomm);//for GNU compilers
 
    MPI_Comm_size(oas3Comm , &amps_size);
    MPI_Comm_rank(oas3Comm , &amps_rank);
