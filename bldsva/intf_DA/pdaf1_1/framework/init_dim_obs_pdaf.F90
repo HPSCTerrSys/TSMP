52e0668feb91608120083c3303c57c31d9ef9630
Johannes Keller <jo.keller@fz-juelich.de>
2023-03-01 12:37:43 +0100

    intf_DA: init_dim_obs_pdaf, small changes
    converge with init_dim_obs_f_pdaf

--- bldsva/intf_DA/pdaf1_1/framework/init_dim_obs_pdaf.F90
+++ bldsva/intf_DA/pdaf1_1/framework/init_dim_obs_pdaf.F90
@@ -83,7 +83,7 @@ SUBROUTINE init_dim_obs_pdaf(step, dim_obs_p)
   !kuw end
   !hcp
   !use the subroutine written by Mukund "domain_def_clm" to evaluate longxy,
-  !latixy, longxy_obs, latixy_obs 
+  !latixy, longxy_obs, latixy_obs
   USE enkf_clm_mod, only: domain_def_clm
   !hcp end
 #endif
@@ -121,7 +121,8 @@ SUBROUTINE init_dim_obs_pdaf(step, dim_obs_p)
   integer :: numl         ! total number of landunits across all processors
   integer :: numc         ! total number of columns across all processors
   integer :: nump         ! total number of pfts across all processors
-  real    :: deltax, deltay !, deltaxy, y1 , x1, z1, x2, y2, z2, R, deltaxy_max
+  real    :: deltax, deltay
+  !real    :: deltaxy, y1 , x1, z1, x2, y2, z2, R, dist, deltaxy_max
 
 
 #endif
@@ -163,13 +164,14 @@ SUBROUTINE init_dim_obs_pdaf(step, dim_obs_p)
 #ifndef CLMSA
 #ifndef OBS_ONLY_CLM
       ! if exist ParFlow-type obs
-        if(allocated(pressure_obs))deallocate(pressure_obs)
+     !if(model == tag_model_parflow) then
+        if(allocated(pressure_obs)) deallocate(pressure_obs)
         allocate(pressure_obs(dim_obs))
         if (multierr.eq.1) then
              if (allocated(pressure_obserr)) deallocate(pressure_obserr)
              allocate(pressure_obserr(dim_obs))
         endif
-        if(allocated(idx_obs_nc))deallocate(idx_obs_nc)
+        if(allocated(idx_obs_nc)) deallocate(idx_obs_nc)
         allocate(idx_obs_nc(dim_obs))
         if(allocated(x_idx_obs_nc))deallocate(x_idx_obs_nc)
         allocate(x_idx_obs_nc(dim_obs))
@@ -177,26 +179,29 @@ SUBROUTINE init_dim_obs_pdaf(step, dim_obs_p)
         allocate(y_idx_obs_nc(dim_obs))
         if(allocated(z_idx_obs_nc))deallocate(z_idx_obs_nc)
         allocate(z_idx_obs_nc(dim_obs))
+     !end if
 #endif
 #endif
 
 #ifndef PARFLOW_STAND_ALONE
 #ifndef OBS_ONLY_PARFLOW
       ! if exist CLM-type obs
-        if(allocated(clm_obs))deallocate(clm_obs)
+!     if(model == tag_model_clm) then
+        if(allocated(clm_obs)) deallocate(clm_obs)
         allocate(clm_obs(dim_obs))
-        if(allocated(clmobs_lon))deallocate(clmobs_lon)
+        if(allocated(clmobs_lon)) deallocate(clmobs_lon)
         allocate(clmobs_lon(dim_obs))
-        if(allocated(clmobs_lat))deallocate(clmobs_lat)
+        if(allocated(clmobs_lat)) deallocate(clmobs_lat)
         allocate(clmobs_lat(dim_obs))
-        if(allocated(clmobs_dr))deallocate(clmobs_dr)
+        if(allocated(clmobs_dr)) deallocate(clmobs_dr)
         allocate(clmobs_dr(2))
-        if(allocated(clmobs_layer))deallocate(clmobs_layer)
+        if(allocated(clmobs_layer)) deallocate(clmobs_layer)
         allocate(clmobs_layer(dim_obs))
         if(multierr.eq.1) then 
-           if(allocated(clm_obserr))deallocate(clm_obserr)               
-           allocate(clm_obserr(dim_obs))
-        end if 
+            if(allocated(clm_obserr)) deallocate(clm_obserr)
+            allocate(clm_obserr(dim_obs))
+        end if
+!     end if
 #endif
 #endif
   end if
