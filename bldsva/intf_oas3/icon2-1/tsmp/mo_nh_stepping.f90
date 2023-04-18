db113cdea80f9b41e01a3d52672da684c55c5161
Stefan Poll <s.poll@fz-juelich.de>
2023-04-18 10:34:34 +0200

    Fix log-file message in ICON

--- bldsva/intf_oas3/icon2-1/tsmp/mo_nh_stepping.f90
+++ bldsva/intf_oas3/icon2-1/tsmp/mo_nh_stepping.f90
@@ -1714,9 +1714,10 @@ MODULE mo_nh_stepping
 
     DO ii = 1, SIZE(oas_snd_meta)
       CALL oasis_put(oas_snd_meta(ii)%vid, sim_time_oas, oas_snd_field(:,ii), oas_error)
-      IF(msg_level > 30 ) &
-        WRITE(oas_message,*) 'Sending  ', oas_snd_meta(ii)%clpname, 'at 0'
-      CALL message(routine, oas_message)
+      IF(msg_level > 30 ) THEN
+        WRITE(oas_message,*) 'Sending  ', oas_snd_meta(ii)%clpname, ' at t=', sim_time_oas
+        CALL message(routine, oas_message)
+      END IF
       IF (oas_error .NE. OASIS_Ok .AND. oas_error .LT. OASIS_Sent) THEN
         WRITE(oas_message,*) 'Failure in oasis_put of ', oas_snd_meta(ii)%clpname
         CALL oasis_abort(oas_comp_id, oas_comp_name, oas_message)
