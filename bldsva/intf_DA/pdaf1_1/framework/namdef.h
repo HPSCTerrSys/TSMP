79bd0c9663d6e9f8197822e3da1e4e27962c0d7a
Johannes Keller <jo.keller@fz-juelich.de>
2023-05-17 17:23:38 +0200

    remove cmem code

--- bldsva/intf_DA/pdaf1_1/framework/namdef.h
+++
@@ -1,3 +0,0 @@
-!*    -----------------------------------------------------------------
-NAMELIST/NAMDEF/ LOFFCMEM, LOFIELDEXP, LGPRINT, JPHISTLEV, CFINOUT, LOMASK_OCEAN, LOMASK_AUTO, INPUTSATINFO
-!     -----------------------------------------------------------------
