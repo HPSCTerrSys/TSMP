82e616f145587d4ca6cfc2f706a08033b9cbbffc
Johannes Keller <jo.keller@fz-juelich.de>
2024-02-27 10:27:04 +0100

    intf_DA: remove `enkf_parflow.h` from wrapper

--- bldsva/intf_DA/pdaf/model/wrapper_tsmp.c
+++ bldsva/intf_DA/pdaf/model/wrapper_tsmp.c
@@ -26,10 +26,6 @@ wrapper_tsmp.c: Wrapper functions for TSMP
 
 #include "enkf.h"
 
-#if defined COUP_OAS_PFL || defined PARFLOW_STAND_ALONE
-#include "enkf_parflow.h"
-#endif
-
 #include "wrapper_tsmp.h"
 
 /*-------------------------------------------------------------------------*/
