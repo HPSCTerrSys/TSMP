b35a9264c156ac9c077b956b67241e492d8351b3
Johannes Keller <jo.keller@fz-juelich.de>
2024-03-19 09:28:50 +0100

    intf_DA: bugfix

--- bldsva/intf_DA/pdaf/framework/collect_state_pdaf.F90
+++ bldsva/intf_DA/pdaf/framework/collect_state_pdaf.F90
@@ -54,6 +54,8 @@ SUBROUTINE collect_state_pdaf(dim_p, state_p)
 ! !USES:
     use mod_tsmp, &
         only: pf_statevec_fortran, tag_model_parflow, tag_model_clm, model
+    use mod_parallel_pdaf, &
+        only: mype_world
 #if defined CLMSA
     !kuw: get access to clm variables
 #if defined CLMFIVE
