79ad0c5f746df372d1fb6140b903f248bad9f708
Johannes Keller <jo.keller@fz-juelich.de>
2024-03-26 09:24:20 +0100

    intf_DA: PE configuration output with grep-hook `Pconf`
    This way the lines of this table can easily be "grepped".
    
    Source:
    https://github.com/larsnerger/NEMO-PDAF/blob/main/MY_SRC_pdaf/parallel_pdaf.F90
    
    Problem: Standard output does not follow the MPI-Barriers.
    
    The OS first writes into process-specific buffers, which are later
    added to the standard output and the OS does not know the order of the
    MPI-Barriers and therefore sometimes does not follow it.

--- bldsva/intf_DA/pdaf/framework/init_parallel_pdaf.F90
+++ bldsva/intf_DA/pdaf/framework/init_parallel_pdaf.F90
@@ -285,20 +285,20 @@ SUBROUTINE init_parallel_pdaf(dim_ens, screen)
   IF (screen > 0) THEN
      IF (mype_world == 0) THEN
         WRITE (*, '(/18x, a)') 'PE configuration:'
-        WRITE (*, '(2x, a6, a9, a10, a14, a13, /2x, a5, a9, a7, a7, a7, a7, a7, /2x, a)') &
-             'world', 'filter', 'model', 'couple', 'filterPE', &
-             'rank', 'rank', 'task', 'rank', 'task', 'rank', 'T/F', &
-             '----------------------------------------------------------'
+        WRITE (*, '(a, 2x, a6, a9, a10, a14, a13, /a, 2x, a5, a9, a7, a7, a7, a7, a7, /a, 2x, a)') &
+          'Pconf', 'world', 'filter', 'model', 'couple', 'filterPE', &
+          'Pconf', 'rank', 'rank', 'task', 'rank', 'task', 'rank', 'T/F', &
+          'Pconf', '----------------------------------------------------------'
      END IF
      CALL MPI_Barrier(MPI_COMM_WORLD, MPIerr)
      IF (task_id == 1) THEN
-        WRITE (*, '(2x, i4, 4x, i4, 4x, i3, 4x, i3, 4x, i3, 4x, i3, 5x, l3)') &
-             mype_world, mype_filter, task_id, mype_model, color_couple, &
-             mype_couple, filterpe
+        WRITE (*, '(a, 2x, i4, 4x, i4, 4x, i3, 4x, i3, 4x, i3, 4x, i3, 5x, l3)') &
+          'Pconf', mype_world, mype_filter, task_id, mype_model, color_couple, &
+          mype_couple, filterpe
      ENDIF
      IF (task_id > 1) THEN
-        WRITE (*,'(2x, i4, 12x, i3, 4x, i3, 4x, i3, 4x, i3, 5x, l3)') &
-         mype_world, task_id, mype_model, color_couple, mype_couple, filterpe
+        WRITE (*, '(a, 2x, i4, 12x, i3, 4x, i3, 4x, i3, 4x, i3, 5x, l3)') &
+          'Pconf', mype_world, task_id, mype_model, color_couple, mype_couple, filterpe
      END IF
      CALL MPI_Barrier(MPI_COMM_WORLD, MPIerr)
 
