1b4cb02b41589acc0fdc1d912f6af4ca01a44632
Johannes Keller <jo.keller@fz-juelich.de>
2024-04-25 10:46:21 +0200

    intf_DA: small doc change

--- bldsva/intf_DA/pdaf/framework/init_ens.F90
+++ bldsva/intf_DA/pdaf/framework/init_ens.F90
@@ -48,7 +48,7 @@ SUBROUTINE init_ens(filtertype, dim_p, dim_ens, state_p, Uinv, &
 ! This routine initializes the PE-local state ensemble with
 ! dummy values that are not used in the first assimilation.
 ! The inital ensemble for a specific component
-! model of TSMP-PDAF is read from a list of input files.
+! model of TSMP-PDAF is read from a sequence of input files.
 !
 ! !REVISION HISTORY:
 ! 2013-02 - Lars Nerger - Initial code
