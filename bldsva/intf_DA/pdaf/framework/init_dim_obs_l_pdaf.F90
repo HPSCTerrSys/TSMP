cc7ba6885952f5c10b69a82c89412bba681f8a26
Johannes Keller <jo.keller@fz-juelich.de>
2024-10-29 17:42:46 +0100

    remove duplicate code in `init_dim_obs_l_pdaf`

--- bldsva/intf_DA/pdaf/framework/init_dim_obs_l_pdaf.F90
+++ bldsva/intf_DA/pdaf/framework/init_dim_obs_l_pdaf.F90
@@ -129,118 +129,9 @@ SUBROUTINE init_dim_obs_l_pdaf(domain_p, step, dim_obs_f, dim_obs_l)
 #endif
 
   ! Count observations within cradius
-#ifdef CLMSA 
-  obsind    = 0
-  obsdist   = 0.0
-  dim_obs_l = 0
-  if(point_obs.eq.0) then
-     max_var_id = MAXVAL(var_id_obs_nc(:,:))
-     allocate(log_var_id(max_var_id))
-     log_var_id(:) = .TRUE.
 
-     do m = 1, dim_nx
-        do k = 1, dim_ny   
-           i = (m-1)* dim_ny + k
-           do j = 1, max_var_id
-              if(log_var_id(j) .and. var_id_obs_nc(k,m) == j) then
-                 dx = abs(longxy_obs(i) - longxy(domain_p))
-                 dy = abs(latixy_obs(i) - latixy(domain_p))
-                 dist = sqrt(real(dx)**2 + real(dy)**2)
-                 !obsdist(i) = dist
-                 if(dist == 0) then
-                    dim_obs_l = dim_obs_l + 1
-                    obsind(i) = 1
-                    log_var_id(j) = .FALSE.
-                    obsdist(i) = dist
-                    EXIT
-                 end if
-              end if
-           end do
-        enddo
-     enddo
-
-     do m = 1, dim_nx
-        do k = 1, dim_ny   
-           i = (m-1)* dim_ny + k
-           do j = 1, max_var_id
-              if(log_var_id(j) .and. var_id_obs_nc(k,m) == j) then
-                 dx = abs(longxy_obs(i) - longxy(domain_p))
-                 dy = abs(latixy_obs(i) - latixy(domain_p))
-                 dist = sqrt(real(dx)**2 + real(dy)**2)
-                 !obsdist(i) = dist
-                 if (dist <= real(cradius)) then
-                    dim_obs_l = dim_obs_l + 1
-                    obsind(i) = 1
-                    log_var_id(j) = .FALSE.
-                    dx = abs(lon_var_id(j) - longxy(domain_p))
-                    dy = abs(lat_var_id(j) - latixy(domain_p))
-                    obsdist(i) = sqrt(real(dx)**2 + real(dy)**2)
-                 end if
-              end if
-           end do
-        enddo
-     enddo
-  else 
-     do i = 1,dim_obs
-        dx = abs(longxy_obs(i) - longxy(domain_p))
-        dy = abs(latixy_obs(i) - latixy(domain_p))
-        dist = sqrt(real(dx)**2 + real(dy)**2)
-        obsdist(i) = dist
-        if (dist <= real(cradius)) then
-           dim_obs_l = dim_obs_l + 1
-           obsind(i) = 1
-        end if
-     end do
-  end if
-#else
-#ifdef PARFLOW_STAND_ALONE
-  obsind    = 0
-  obsdist   = 0.0
-  dim_obs_l = 0
-  if(point_obs.eq.0) then
-     max_var_id = MAXVAL(var_id_obs_nc(:,:))
-     allocate(log_var_id(max_var_id))
-     log_var_id(:) = .TRUE.
-
-     do m = 1, dim_nx
-        do k = 1, dim_ny   
-           i = (m-1)* dim_ny + k
-           do j = 1, max_var_id
-              if(log_var_id(j) .and. var_id_obs_nc(k,m) == j) then
-                 dx = abs(x_idx_obs_nc(i) - int(xcoord_fortran(domain_p_coord))-1)
-                 dy = abs(y_idx_obs_nc(i) - int(ycoord_fortran(domain_p_coord))-1)
-                 dist = sqrt(real(dx)**2 + real(dy)**2)
-                 !obsdist(i) = dist
-                 if (dist <= real(cradius) .AND. dist > 0) then
-                    dim_obs_l = dim_obs_l + 1
-                    obsind(i) = 1
-                    log_var_id(j) = .FALSE.
-                    dx = abs(ix_var_id(j) - int(xcoord_fortran(domain_p_coord))-1)
-                    dy = abs(iy_var_id(j) - int(ycoord_fortran(domain_p_coord))-1)
-                    obsdist(i) = sqrt(real(dx)**2 + real(dy)**2)
-                 else if(dist == 0) then
-                    dim_obs_l = dim_obs_l + 1
-                    obsind(i) = 1
-                    log_var_id(j) = .FALSE.
-                    obsdist(i) = dist  
-                 end if
-              end if
-           end do
-        end do
-     end do
-  else   
-        do i = 1,dim_obs
-           dx = abs(x_idx_obs_nc(i) - int(xcoord_fortran(domain_p_coord))-1)
-           dy = abs(y_idx_obs_nc(i) - int(ycoord_fortran(domain_p_coord))-1)
-           dist = sqrt(real(dx)**2 + real(dy)**2)
-           obsdist(i) = dist
-           if (dist <= real(cradius)) then
-              dim_obs_l = dim_obs_l + 1
-              obsind(i) = 1
-           end if
-        end do
-  endif
-#else
+#ifndef CLMSA
+#ifndef OBS_ONLY_CLM
   obsind    = 0
   obsdist   = 0.0
   dim_obs_l = 0
@@ -276,7 +167,33 @@ SUBROUTINE init_dim_obs_l_pdaf(domain_p, step, dim_obs_f, dim_obs_l)
            end do
         end do
      end do
-     end if 
+     end if
+  else   
+     if(model == tag_model_parflow) THEN
+        do i = 1,dim_obs
+           dx = abs(x_idx_obs_nc(i) - int(xcoord_fortran(domain_p_coord))-1)
+           dy = abs(y_idx_obs_nc(i) - int(ycoord_fortran(domain_p_coord))-1)
+           dist = sqrt(real(dx)**2 + real(dy)**2)
+           obsdist(i) = dist
+           if (dist <= real(cradius)) then
+              dim_obs_l = dim_obs_l + 1
+              obsind(i) = 1
+           end if
+        end do
+     endif
+  endif
+#endif
+#endif
+
+#ifndef PARFLOW_STAND_ALONE
+#ifndef OBS_ONLY_PARFLOW
+  obsind    = 0
+  obsdist   = 0.0
+  dim_obs_l = 0
+  if(point_obs.eq.0) then
+     max_var_id = MAXVAL(var_id_obs_nc(:,:))
+     allocate(log_var_id(max_var_id))
+     log_var_id(:) = .TRUE.
 
      if(model == tag_model_clm) THEN
      do m = 1, dim_nx
@@ -322,22 +239,7 @@ SUBROUTINE init_dim_obs_l_pdaf(domain_p, step, dim_obs_f, dim_obs_l)
         enddo
      enddo
      end if
-     ! print *,'mype_filter dim_obs_l ', mype_filter, dim_obs_l
-  else   
-     if(model == tag_model_parflow) THEN
-        do i = 1,dim_obs
-           dx = abs(x_idx_obs_nc(i) - int(xcoord_fortran(domain_p_coord))-1)
-           dy = abs(y_idx_obs_nc(i) - int(ycoord_fortran(domain_p_coord))-1)
-           dist = sqrt(real(dx)**2 + real(dy)**2)
-           obsdist(i) = dist
-           if (dist <= real(cradius)) then
-              dim_obs_l = dim_obs_l + 1
-              obsind(i) = 1
-           end if
-        end do
-     endif
-
-#ifndef OBS_ONLY_PARFLOW     
+  else 
      if(model == tag_model_clm) THEN
      do i = 1,dim_obs
         dx = abs(longxy_obs(i) - longxy(domain_p))
@@ -348,13 +250,13 @@ SUBROUTINE init_dim_obs_l_pdaf(domain_p, step, dim_obs_f, dim_obs_l)
            dim_obs_l = dim_obs_l + 1
            obsind(i) = 1
         end if
-        end do
+     end do
      end if 
-#endif
-  endif
+  end if
 #endif
 #endif  
-  
+!------------------------------------------------------------------------
+
   ! kuw: allocate and determine local observation index and distance
   !#ifndef CLMSA
   ! Initialize index array for local observations in full observed vector
