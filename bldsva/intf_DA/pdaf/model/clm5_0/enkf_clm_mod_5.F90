6cb013648a0c1cb005948d279e9bbba88470298b
Johannes Keller <jo.keller@fz-juelich.de>
2024-12-04 16:24:45 +0100

    bugfix: missing pointer declarations
    ```
    clm5_0/enkf_clm_mod_5.F90(372): error #6404: This name does not have a
    type, and must have an explicit type.   [T_GRND]
    ```

--- bldsva/intf_DA/pdaf/model/clm5_0/enkf_clm_mod_5.F90
+++ bldsva/intf_DA/pdaf/model/clm5_0/enkf_clm_mod_5.F90
@@ -360,6 +360,9 @@ module enkf_clm_mod
     real(r8), pointer :: psand(:,:)
     real(r8), pointer :: pclay(:,:)
     real(r8), pointer :: porgm(:,:)
+    real(r8), pointer :: t_grnd(:)
+    real(r8), pointer :: t_veg(:)
+    real(r8), pointer :: tlai(:)
     integer :: i,j,jj,g,cc=0,offset=0
     character (len = 34) :: fn    !TSMP-PDAF: function name for state vector output
     character (len = 34) :: fn2    !TSMP-PDAF: function name for swc output
@@ -476,6 +479,9 @@ module enkf_clm_mod
     real(r8), pointer :: psand(:,:)
     real(r8), pointer :: pclay(:,:)
     real(r8), pointer :: porgm(:,:)
+
+    real(r8), pointer :: t_grnd(:)
+    real(r8), pointer :: t_veg(:)
 
     real(r8), pointer :: dz(:,:)          ! layer thickness depth (m)
     real(r8), pointer :: h2osoi_liq(:,:)  ! liquid water (kg/m2)
